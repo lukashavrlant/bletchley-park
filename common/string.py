ef6dbd1abd7a6cb2a19e36be7b38ca36b3fe771e
Lukáš Havrlant <lukas.havrlant@gmail.com>
2012-09-29 10:44:02 +0200

    function renamed

--- common/string.py
+++ common/string.py
@@ -3,7 +3,7 @@ import unicodedata
 def _strip_accents(text):
 	return str(''.join((c for c in unicodedata.normalize('NFD', text) if unicodedata.category(c) != 'Mn')))
 
-def normalize_test(text):
+def normalize_text(text):
 	text = _strip_accents(unicode(text)).lower()
 	text = filter(_keep_char, text)
 	return text
