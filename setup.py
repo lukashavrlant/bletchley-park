3a4cc7036db190768f2e1b8fd2feff128842f17f
Lukáš Havrlant <lukas.havrlant@gmail.com>
2012-09-30 22:38:39 +0200

    + setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(name='bletchleypark',
+      version='1.0',
+      packages = ['bletchleypark', 'bletchleypark.ciphers', 'bletchleypark.cryptanalysis', 'bletchleypark.common'],
+      package_data = {'bletchleypark' : ['data/lang/*.zip']},
+      scripts = ["bletchleypark/bpark"]
+      )
