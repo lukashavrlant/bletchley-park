3ac54c696158efc3577d14eb85a8858097ac32e9
Lukáš Havrlant <lukas.havrlant@gmail.com>
2012-09-30 18:51:48 +0200

    + test triangle attack

---
+++ tests/cryptanalysis_tests/test_triangle_attack.py
@@ -0,0 +1,18 @@
+import unittest
+from ciphers.caesar import Caesar
+from cryptanalysis.triangle_attack import TriangleAttack
+from common.string import normalize_text
+from common.lang_model import LangModel
+from common.files import get_lang_path
+from cryptanalysis.language_stats import LanguageStats
+
+class TestTriangleForce(unittest.TestCase):
+	def test_cs_crack(self):
+		cipher = Caesar()
+		cracker = TriangleAttack()
+		open_text = normalize_text("Ja, Syn Poklopu, rozzuren do silenstvi, vodarnu srovnam se zemi, jmenem mistru kanvodstvi. Ja, matku vlastni, vsak kdo vi zda, prokouknutou mel, nikoliv jak pribuzni. Tak jako davno pred casem jsme svatou chatru topily, stejny konec pripravime podvodnikum z vodarny. Nelze verit nikomu, kdo ma tlamu plnou cistoty. Vsechny tyhle bestie ceka voda ze stoky.")
+		key = "q"
+		cipher_text = cipher.encrypt(open_text, key)
+		langmodel = LangModel(get_lang_path('cs'))
+		cracked_key = cracker.crack(cipher_text, LanguageStats(langmodel))
+		self.assertEqual(cracked_key, key)
