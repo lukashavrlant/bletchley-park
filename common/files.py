ba0ee01cfe5a8b4c62b5250a0a5555a7096af0b8
Lukáš Havrlant <lukas.havrlant@gmail.com>
2012-09-30 19:55:41 +0200

    +help functions

--- common/files.py
+++ common/files.py
@@ -4,3 +4,17 @@ DATA_DIRECTORY = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(i
 
 def get_lang_path(lang):
 	return DATA_DIRECTORY + "/lang/%s.zip" % lang
+
+def readfile(path):
+	with open(path, "r") as f:
+		return f.read()
+
+def savefile(path, content):
+	with open(path, "w") as f:
+		f.write(content)
+
+def send_to_output(content, output):
+	if output:
+		savefile(output, content)
+	else:
+		print content
