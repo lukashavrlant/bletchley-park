1dc71889389d42aa904c9866e898591420d98ff3
Lukáš Havrlant <lukas.havrlant@gmail.com>
2012-09-30 18:46:02 +0200

    + test caesar brute force method

--- tests/cryptanalysis_tests/test_caesar_brute_force.py
+++ tests/cryptanalysis_tests/test_caesar_brute_force.py
@@ -2,14 +2,17 @@ import unittest
 from ciphers.caesar import Caesar
 from cryptanalysis.caesar_brute_force import CaesarBruteForce
 from common.string import normalize_text
+from common.lang_model import LangModel
+from common.files import get_lang_path
+from cryptanalysis.language_stats import LanguageStats
 
 class TestCaesarBruteForce(unittest.TestCase):
-	def test_crack(self):
+	def test_cs_crack(self):
 		cipher = Caesar()
 		cracker = CaesarBruteForce()
 		open_text = normalize_text("Python is a general-purpose, interpreted high-level programming language")
 		key = "d"
-		# cipher_text = cipher.encrypt(open_text, key)
-		# cracked_key = cracker.crack(cipher_text)
-		# self.assertEqual(cracked_key, key)
-		
+		cipher_text = cipher.encrypt(open_text, key)
+		langmodel = LangModel(get_lang_path('cs'))
+		cracked_key = cracker.crack(cipher_text, LanguageStats(langmodel))
+		self.assertEqual(cracked_key, key)	
