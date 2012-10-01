d1969672f68a0a16d8bd29fcb522fc211cf3f667
Lukáš Havrlant <lukas.havrlant@gmail.com>
2012-10-01 12:03:59 +0200

    data directory changed

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from distutils.core import setup
 setup(name='bletchleypark',
       version='1.0',
       packages = ['bletchleypark', 'bletchleypark.ciphers', 'bletchleypark.cryptanalysis', 'bletchleypark.common'],
-      package_data = {'bletchleypark' : ['data/lang/*.zip']},
+      package_data = {'bletchleypark' : ['../data/lang/*.zip']},
       scripts = ["bletchleypark/bletchleypark"]
       )
