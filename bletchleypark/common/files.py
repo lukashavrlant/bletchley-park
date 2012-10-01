0ea33ac3182d277855671d82196f8c7248fb9f4e
Lukáš Havrlant <lukas.havrlant@gmail.com>
2012-10-01 12:08:45 +0200

    data directory changed

--- bletchleypark/common/files.py
+++ bletchleypark/common/files.py
@@ -1,6 +1,6 @@
 import inspect, os
 
-DATA_DIRECTORY = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))), "../../data"))
+DATA_DIRECTORY = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))), "../data"))
 
 def get_lang_path(lang):
 	return DATA_DIRECTORY + "/lang/%s.zip" % lang
